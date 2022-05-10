612cd81bdf80d84b2a5419a83645aa0d4486e33b
Walber Jefferson <walberjefferson@hotmail.com>
2022-05-10 16:10:49 -0300

    Montado classe de cessao

---
+++ test/cessao.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Attiva\Siapxml\FolhaPagamento\Cessao;
+
+$dados = [
+    [
+        'cpf' => '21208138090',
+        'matricula' => 'jWBmuGXpYGuTmRqv',
+        'processo' => '06/2021',
+        'numero_ato' => '25/2021',
+        'data_ato' => '2021-05-08',
+        'veiculo_publicacao' => '4',
+        'data_inicio' => '2021-06-06',
+        'data_fim' => '2016-02-02',
+        'onus' => '2',
+        'cnpj_cedente' => '08103219000128',
+        'cnpj_cessionario' => '87556227000104',
+    ]
+];
+
+$adicional = new Cessao('60', '2022', '01', $dados);
+$adicional->processar()->save('/../xml/Vida Funcional');
