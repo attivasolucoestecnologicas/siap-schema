7439dd3b1d381b214d540fb5581b052225d627af
Walber Jefferson <walberjefferson@hotmail.com>
2022-05-10 16:14:42 -0300

    Montado classe de classe

---
+++ test/classe.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Attiva\Siapxml\FolhaPagamento\Classe;
+
+$dados = [
+    [
+        'codigo' => '567',
+        'nome' => 'Especial A',
+        'tempo_servico_minimo' => '5475',
+    ],
+    [
+        'codigo' => '566',
+        'nome' => 'Especial B',
+        'tempo_servico_minimo' => '5475',
+    ],
+    [
+        'codigo' => '565',
+        'nome' => 'Especial C',
+        'tempo_servico_minimo' => '1825',
+    ]
+];
+
+$adicional = new Classe('60', '2022', '01', $dados);
+$adicional->processar()->save('/../xml/Vida Funcional');
