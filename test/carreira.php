f53c10d9f47b2b7dc912b7c7e645a5ec2c7e97b9
Walber Jefferson <walberjefferson@hotmail.com>
2022-05-10 16:05:57 -0300

    Montado classe de carreira

---
+++ test/carreira.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use Attiva\Siapxml\FolhaPagamento\Carreira;
+
+
+$dados = [
+    [
+        'codigo' => '287',
+        'nome' => 'Servidor',
+        'data_criacao' => '1998-10-05',
+        'data_ato_criacao' => '2003-11-02',
+        'ato_criacao' => '145/2000',
+        'veiculo_publicacao_ato_criacao' => '1',
+        'alteracao_nomeclatura' => '',
+        'data_alteracao' => '',
+        'data_ato_alteracao' => '',
+        'ato_alteracao' => '',
+        'veiculo_publicacao_ato_alteracao' => '',
+        'data_extincao' => '',
+        'data_ato_extincao' => '',
+        'ato_extincao' => '',
+        'veiculo_publicacao_ato_extincao' => '',
+    ],
+];
+
+$adicional = new Carreira('60', '2022', '01', $dados);
+$adicional->processar()->save('/../xml/Vida Funcional');
